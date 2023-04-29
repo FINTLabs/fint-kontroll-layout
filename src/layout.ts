4c7d19a439dafa682a370b0168f695f824c68498
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-04-29 20:36:06 +0200

    Wip

--- src/layout.ts
+++ src/layout.ts
@@ -1,4 +1,5 @@
 import {DefaultOptions, startLayout} from "@fintlabs/flais-podium-layout";
 
+const podsFile = process.env.PODS_FILE || `${process.cwd()}/pods-local.json`;
 
-startLayout(new DefaultOptions("fint-kontroll-layout", `${process.cwd()}/pods-local.json`));
+startLayout(new DefaultOptions("fint-kontroll-layout",  podsFile));
