1be4a1b1729a16f611643f25e8c06cf082bb9664
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-01 18:02:42 +0200

    Lets see if this works

--- src/layout.ts
+++ src/layout.ts
@@ -1,5 +1,5 @@
-import {DefaultOptions, startLayout} from "@fintlabs/flais-podium-layout";
+import {startLayout} from "@fintlabs/flais-podium-layout";
 
-const podsFile = process.env.PODS_FILE || `${process.cwd()}/pods.json`;
+const layoutConfigurationUri = process.env.LAYOUT_CONFIGURATION_URI || `${process.cwd()}/pods.json`;
 
-startLayout(new DefaultOptions("fint-kontroll-layout",  podsFile));
+startLayout(layoutConfigurationUri);
