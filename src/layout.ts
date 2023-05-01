4d534ac5737b237f7dadd6d5b741b9a03e713d44
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-01 21:00:53 +0200

    💩

--- src/layout.ts
+++ src/layout.ts
@@ -1,5 +1,6 @@
 import {startLayout} from "@fintlabs/flais-podium-layout";
 
-const layoutConfigurationUri = process.env.LAYOUT_CONFIGURATION_URI || `${process.cwd()}/pods.json`;
-
-startLayout(layoutConfigurationUri);
+startLayout(
+    process.env.LAYOUT_CONFIGURATION_URI
+    || `${process.cwd()}/pods.json`
+);
